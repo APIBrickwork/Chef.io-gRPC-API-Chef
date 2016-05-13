e485fa498e0ec832bdcc7a3c4eac55fd8da6b8f1
Tobias Freundorfer <tobiasfreundorfer@gmail.com>
2016-05-13 10:37:04 +0200

    aws_provisioning now uses specific (self generated) ssh keys. Updated initScript to build empty .ssh folder in server folder.

--- cookbooks/cb-chefmate/recipes/aws_provision.rb
+++ cookbooks/cb-chefmate/recipes/aws_provision.rb
@@ -1,28 +1,40 @@
 #
-# Cookbook Name:: cb-chefmate
+# Cookbook Name:: Chefmate
 # Recipe:: aws_provision
-# Author: Tobias Freundorfer
+#
 #
 
 # Use AWS to provision to
 require 'chef/provisioning/aws_driver'
 
+# Using Ruby to get home-directory
+home = File.expand_path('~') 
+
 with_driver "aws::#{node['chefmate']['machine']['region']}" do
 
-	with_machine_options({
-		bootstrap_options: {
-    	# http://docs.aws.amazon.com/sdkforruby/api/Aws/EC2/Resource.html#create_instances-instance_method
-    	# lists the available options.  The below options are the default
-    	image_id: "#{node['chefmate']['machine']['imageid']}",
-    	instance_type: "#{node['chefmate']['machine']['instancetype']}",
-    	security_group_ids: "#{node['chefmate']['machine']['defaultsecuritygroupdids']}"
-  		}
-	})
-	
-	machine "#{node['chefmate']['machine']['name']}" do
-   		tag "#{node['chefmate']['machine']['tag']}"
-   		action :allocate
-	end
+  # Create ssh key-pair
+  aws_key_pair 'chefmateserver_key' do
+    private_key_path "#{home}/chefmateserver/.ssh/chefmateserver_key.pem"
+    public_key_path "#{home}/chefmateserver/.ssh/chefmateserver_key.pub"
+    allow_overwrite true # Regenerates the key every run!
+  end
+
+  with_machine_options({
+    bootstrap_options: {
+      # http://docs.aws.amazon.com/sdkforruby/api/Aws/EC2/Resource.html#create_instances-instance_method
+      # lists the available options.  The below options are the default
+      image_id: "#{node['chefmate']['machine']['imageid']}",
+      instance_type: "#{node['chefmate']['machine']['instancetype']}",
+      security_group_ids: "#{node['chefmate']['machine']['defaultsecuritygroupdids']}",
+      key_name: 'chefmateserver_key',
+      key_path: "#{home}/chefmateserver/.ssh"
+      }
+  })
+  
+  machine "#{node['chefmate']['machine']['name']}" do
+      tag "#{node['chefmate']['machine']['tag']}"
+      action :allocate
+  end
 
 
 end
