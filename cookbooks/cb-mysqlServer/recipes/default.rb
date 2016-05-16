660e44e80016a0972df07a36e8eece446f1b7d26
Tobias Freundorfer <tobiasfreundorfer@gmail.com>
2016-05-16 14:29:58 +0200

    Next try with granting user permissions

--- cookbooks/cb-mysqlServer/recipes/default.rb
+++ cookbooks/cb-mysqlServer/recipes/default.rb
@@ -15,6 +15,19 @@ end
 mysql_service "#{node['chefmate']['machine']['mysql']['servicename']}" do
   port "#{node['chefmate']['machine']['mysql']['port']}"
   version '5.5'
+  bind_address '0.0.0.0'
   initial_root_password "#{node['chefmate']['machine']['mysql']['rootpw']}"
   action [:create, :start]
+end
+
+mysql_database_user 'wp' do
+  connection(
+    :host => '127.0.0.1',
+    :username => 'wp',
+    :password => "#{node['chefmate']['machine']['mysql']['rootpw']}"
+  )
+  password 'wppw'
+  database_name 'wordpressdb'
+  host '%'
+  action [:create, :grant]
 end
