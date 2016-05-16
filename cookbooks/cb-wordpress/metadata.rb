55b0f9b1785638d4037586ca6dac402b566fdbec
Tobias Freundorfer <tobiasfreundorfer@gmail.com>
2016-05-16 09:26:52 +0200

    Added wrapper cookbook for wordpress

---
+++ cookbooks/cb-wordpress/metadata.rb
@@ -0,0 +1,9 @@
+name             'cb-wordpress'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures cb-wordpress'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'wordpress', '~> 3.0.0'
