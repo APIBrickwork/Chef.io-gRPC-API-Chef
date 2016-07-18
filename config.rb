41bf6c4324f6f64311e1bab56a9d3404d54a812e
Tobias Freundorfer <tobiasfreundorfer@gmail.com>
2016-07-18 18:56:20 +0200

    Fixed wrong path

--- config.rb
+++ config.rb
@@ -1,3 +1,3 @@
 home = File.expand_path('~')
 file_cache_path "#{home}/chef-solo"
-cookbook_path "#{home}/chefmateserver/Chef.io-gRPC-API-Chef/cookbooks"
+cookbook_path "#{home}/git/Chef.io-gRPC-API-Chef/cookbooks"
